198581e9cc1ad1edc962620e87c2469341c9cfbe
AyaLahlou <50680354+AyaLahlou@users.noreply.github.com>
2024-12-18 11:46:59 -0500

    push train.sh

---
+++ src/train.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+# Simple "Hello World" submit script for Slurm.
+#
+# Replace ACCOUNT with your account name before submitting.
+#
+#SBATCH --account=glab        # Replace ACCOUNT with your group account name
+#SBATCH --job-name=train    # The job name
+#SBATCH --gres=gpu:1
+#SBATCH -c 2                    # The number of cpu cores to use (up to 32 cores per server)
+#SBATCH --time=0-15:00            # The time the job will take to run in D-HH:MM
+#SBATCH --mem-per-cpu=64gb         # The memory the job will use per cpu core
+ 
+module load anaconda
+module load cuda11.1/toolkit
+#Command to execute Python program
+pip install tft-torch
+ 
+#first train boreal 2
+python train_tft.py NDT.pickle
+
+# End of script
